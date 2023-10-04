21cd912741a56c254aba1ecfd125ab1478d46875
StealthWorm <thierrypitela@hotmail.com>
2023-10-04 14:35:29 -0300

    feat: build

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,12 +4,7 @@ import { DefaultLayout } from './layouts/DefaultLayout'
 import { Home } from './pages/Home'
 import { Post } from './pages/Post'
 
-import { useContextSelector } from 'use-context-selector'
-import { IssuesContext } from './contexts/IssuesContext'
-
 export function Router() {
-  const issues = useContextSelector(IssuesContext, (context) => context.issues)
-
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
